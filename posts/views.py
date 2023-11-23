a3562717e47c6dba20c984b9e26e6ae2dfcf8181
Chey <ctenmanikdavid@gmail.com>
2023-11-23 03:41:06 +0000

    Add filters to profiles and comments, update requirements.txt

--- posts/views.py
+++ posts/views.py
@@ -27,6 +27,7 @@ class PostList(generics.ListCreateAPIView):
         "owner__followed__owner__profile",
         "likes__owner__profile",
         "owner__profile",
+        "posts",
     ]
     search_fields = [
         "owner__username",
