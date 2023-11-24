660da7ea94c99793450cda306efd479ad90dae50
Chey <ctenmanikdavid@gmail.com>
2023-11-24 07:57:10 +0000

    spell correction in installed apps

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -89,7 +89,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "dj_rest_auth.registration",
-    "corseaders",
+    "corsheaders",
     "profiles",
     "posts",
     "comments",
