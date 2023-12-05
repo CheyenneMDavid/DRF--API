cfa82b4d3ca1b8c8e10d4b5885350f0385ccb2ce
Chey <ctenmanikdavid@gmail.com>
2023-12-05 11:31:15 +0000

    Update CORS configuration to include localhost and dynamic Gitpod URLs

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -108,17 +108,22 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+# Static addition for local development using vscode.
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 if "CLIENT_ORIGIN_DEV" in os.environ:
     extracted_url = re.match(
         r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
     ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-        "CLIENT_ORIGIN_DEV_VSCODE",
     ]
 
 
 CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = "drf_api.urls"
 
