96ecdbb5a0cd3efd6c94bbef5032ee93462403bf
Chey <ctenmanikdavid@gmail.com>
2023-12-11 23:58:24 +0000

    Update image field settings for media storage

--- posts/models.py
+++ posts/models.py
@@ -29,16 +29,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-
     image = models.ImageField(
-        upload_to="images/",
-        image=models.ImageField(upload_to="images/", default="../default_post_ewhfsq"),
-        blank=True,
+        upload_to="images/", default="../default_post_rgq6aq", blank=True
     )
-
-    # image = models.ImageField(
-    #     upload_to="images/", default="../default_profile_rgq6aq", blank=True
-    # )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default="normal"
     )
