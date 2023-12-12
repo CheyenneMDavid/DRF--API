de8fcf9bb645722eca4e8906cbbbfe4d419a5b22
Chey <ctenmanikdavid@gmail.com>
2023-12-12 00:43:21 +0000

    Pasted source code

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,52 +1,41 @@
 from rest_framework import serializers
-from posts.models import Post
-from likes.models import Like
+from .models import Profile
+from followers.models import Follower
 
 
-class PostSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
-    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
-    like_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
-
-    def validate_image(self, value):
-        if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError("Image size larger than 2MB!")
-        if value.image.height > 4096:
-            raise serializers.ValidationError("Image height larger than 4096px!")
-        if value.image.width > 4096:
-            raise serializers.ValidationError("Image width larger than 4096px!")
-        return value
+    following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
 
-    def get_like_id(self, obj):
+    def get_following_id(self, obj):
         user = self.context["request"].user
         if user.is_authenticated:
-            like = Like.objects.filter(owner=user, post=obj).first()
-            return like.id if like else None
+            following = Follower.objects.filter(owner=user, followed=obj.owner).first()
+            # print(following)
+            return following.id if following else None
         return None
 
     class Meta:
-        model = Post
+        model = Profile
         fields = [
             "id",
             "owner",
-            "is_owner",
-            "profile_id",
-            "profile_image",
             "created_at",
             "updated_at",
-            "title",
+            "name",
             "content",
             "image",
-            "image_filter",
-            "like_id",
-            "likes_count",
-            "comments_count",
+            "is_owner",
+            "following_id",
+            "posts_count",
+            "followers_count",
+            "following_count",
         ]
