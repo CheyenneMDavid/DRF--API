8796efe4b2076383283dd0d2b40b87715481f67c
Chey <ctenmanikdavid@gmail.com>
2023-12-12 08:25:46 +0000

    Add CLIENT_ORIGIN_DEV to settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -77,10 +77,6 @@ ALLOWED_HOSTS = [
     ".gitpod.io",
 ]
 
-
-if "CLIENT_ORIGIN" in os.environ:
-    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
-
 if "CLIENT_ORIGIN_DEV" in os.environ:
     extracted_url = re.match(
         r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
