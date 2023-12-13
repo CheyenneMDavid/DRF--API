3e8c73cf36251142faa0cb395e288f428965c34a
Chey <ctenmanikdavid@gmail.com>
2023-12-13 01:07:28 +0000

    Change ALLOWED_HOSTS from url to key and value in environtal configs

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -73,8 +73,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = "DEV" in os.environ
 
 ALLOWED_HOSTS = [
-    # os.environ.get("ALLOWED_HOST"),
-    "drf---api-65413badfaf5.herokuapp.com",
+    os.environ.get("ALLOWED_HOST"),
     "localhost",
     "127.0.0.1",
     ".gitpod.io",
@@ -122,11 +121,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-if "CLIENT_ORIGIN" in os.environ:
-    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
-else:
+if "CLIENT_ORIGIN_DEV" in os.environ:
+    extracted_url = re.match(
+        r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
