5c6426d5a19fe99423b8e24734cfe94b842e9c13
Chey <ctenmanikdavid@gmail.com>
2024-01-02 23:49:47 +0000

    Update CORS settings for frontend access

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -131,10 +131,10 @@ if "CLIENT_ORIGIN_DEV" in os.environ:
 CORS_ALLOW_CREDENTIALS = True
 
 
-# Static addition for local development using vscode.
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://momentos-82c8916fc192.herokuapp.com",
+]
 
 
 ROOT_URLCONF = "drf_api.urls"
