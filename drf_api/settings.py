7507b2bb507ed7fd1310ca889086073a62e14c49
Chey <ctenmanikdavid@gmail.com>
2024-01-28 17:57:41 +0000

    Commented out local hosts in allowed

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -74,8 +74,8 @@ DEBUG = "DEV" in os.environ
 ALLOWED_HOSTS = [
     os.environ.get("ALLOWED_HOST"),
     "localhost",
-    "127.0.0.1",
-    ".gitpod.io",
+    # "127.0.0.1",
+    # ".gitpod.io",
 ]
 
 
